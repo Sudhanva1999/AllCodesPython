80dd7758d852d7b996ec8eba48b625608181afd0
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2020-08-05 15:03:24 +0530

    Update SelectionSort.py

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -6,9 +6,7 @@ def selection_sort(arr):
                 m=arr[j]
                 pos=j
         arr[i],arr[pos]=m,arr[i]
-        print(arr[i],arr[pos])
 
-        
 print('Enter space separated Elements.')
 arr=list(map(int,input().split()))
 selection_sort(arr)
